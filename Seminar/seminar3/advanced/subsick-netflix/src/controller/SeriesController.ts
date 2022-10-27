472f2b41e29b9634e8d71eeeb713572a140a55c4
ehdwoKIM <kinbell19@gmail.com>
2022-10-27 22:41:51 +0900

    [3주차]도전 과제 제출

--- Seminar/seminar3/advanced/subsick-netflix/src/controller/SeriesController.ts
+++ Seminar/seminar3/advanced/subsick-netflix/src/controller/SeriesController.ts
@@ -17,6 +17,75 @@ const getSeries = async (req: Request, res: Response) => {
     return res.status(200).json({status: 200, message: "회차 정보 조회 성공", data});
 };
 
+/**
+ * @route POST /toMyList/:seriesId
+ * @desc 찜 버튼 누르기
+ * @access Public
+ */
+const toMyList = async (req: Request, res: Response) => {
+    const { seriesId } = req.params;
+    
+    const data = await SeriesService.toMyList(seriesId);
+    
+    if (!data) {
+        return res.status(404).json({
+            status: 404, message: "NOT_FOUND"});
+    }
+
+    return res.status(200).json({status: 200, message: "찜 하기 성공", data});
+};
+
+/**
+ * @route Delete /notMyList/:seriesId
+ * @desc 찜 버튼 취소하기
+ * @access Public
+ */
+ const notMyList = async (req: Request, res: Response) => {
+    const { seriesId } = req.params;
+    
+    const data = await SeriesService.notMyList(seriesId);
+    
+    if (!data) {
+        return res.status(404).json({
+            status: 404, message: "NOT_FOUND"});
+    }
+
+    return res.status(200).json({status: 200, message: "찜 취소 성공", data});
+};
+
+/**
+ * @route POST /isEvaluate/:seriesId
+ * @desc 평가하기
+ * @access Public
+ */
+ const isEvaluate = async (req: Request, res: Response) => {
+    const { seriesId } = req.params;
+    const { evalId } = req.body;
+    
+    // interface EvalResponse {
+    //     evalId: string;
+    // };
+
+    // const responseData: EvalResponse = {
+    //     evalId: req.body.evalId
+    // };
+    
+    const data = await SeriesService.createEval(seriesId, evalId);
+
+    if (!data) {
+        return res.status(404).json({
+            status: 404, message: "NOT_FOUND"});
+    }
+    
+    return res.status(200).json({
+        status: 200, message: "작품 평가 성공",data});
+};
+
+
+
 export default{
     getSeries,
+    toMyList,
+    notMyList,
+    isEvaluate,
 };
--- Seminar/seminar3/advanced/subsick-netflix/src/service/SeriesService.ts
+++ Seminar/seminar3/advanced/subsick-netflix/src/service/SeriesService.ts
@@ -1,10 +1,33 @@
-//const data = require("./../database/data.json");
+const data = require("./../database/data.json");
 
 const getSeries = async (seriesId: string) => {
-    const data = require("./../database/data.json");
+    return data[seriesId];
+};
+
+const toMyList = async (seriesId: string) => {
+    
+    data[seriesId].toMyList = true;
+    
+    return data[seriesId];
+};
+
+const notMyList = async (seriesId: string) => {
+    
+    data[seriesId].toMyList = false;
+    
+    return data[seriesId];
+};
+
+const createEval = async (seriesId: string, evalId: string) => {
+    
+    data[seriesId].evalId = evalId;
+
     return data[seriesId];
 };
 
 export default {
     getSeries,
+    toMyList,
+    notMyList,
+    createEval
 };
